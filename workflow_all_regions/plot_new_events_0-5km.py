860ebfc1a8e804071eeb4fd93bb29010fba44fb7
Hiroto Bito <hbito@psf-nvme01-prd-j375.ess.washington.edu>
2024-11-14 03:08:25 -0800

    commit

---
+++ workflow_all_regions/plot_new_events_0-5km.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import obspy
+from tqdm import tqdm
+
+from obspy.clients.fdsn import Client
+from obspy.clients.fdsn import Client as FDSNClient
+
+from mpl_toolkits.basemap import Basemap
+
+
+from pnwstore.mseed import WaveformClient
+from datetime import datetime
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+from obspy import UTCDateTime
+
+from tqdm import tqdm
+from obspy.geodetics import locations2degrees, degrees2kilometers
+
+notebook_dir = os.getcwd()
+parent_dir = os.path.abspath(os.path.join(notebook_dir, '../'))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+from plot_utils import *
+
+# Load all the pick assignments 
+year = 'all_regions'
+
+# Load all event data into a list of DataFrames
+new_events = pd.read_csv(f'../data/datasets_{year}/new_events.csv')
+
+# Pick only the events less than 5 km deep
+new_events = new_events[new_events['depth'] < 5]
+
+# Load all pick_assignments data into a list of DataFrames
+pick_assignment_files = [
+    '../data/datasets_nwa_shelf_trench/all_pick_assignments_nwa_shelf_trench.csv',
+    '../data/datasets_nwa_shore/all_pick_assignments_nwa_shore.csv',
+    '../data/datasets_or_shelf_trench/all_pick_assignments_or_shelf_trench.csv',
+    '../data/datasets_or_shore/all_pick_assignments_or_shore.csv',
+    '../data/datasets_pnsn_jdf/all_pick_assignments_pnsn_jdf.csv',
+    '../data/datasets_pnsn_nor/all_pick_assignments_pnsn_nor.csv',
+    '../data/datasets_pnsn_sor/all_pick_assignments_pnsn_sor.csv',
+    '../data/datasets_pnsn_wa/all_pick_assignments_pnsn_wa.csv',
+    '../data/datasets_swa_shelf_trench/all_pick_assignments_swa_shelf_trench.csv',
+    '../data/datasets_swa_shore/all_pick_assignments_swa_shore.csv'
+]
+
+# Read all event files into DataFrames
+dfs = [pd.read_csv(file, index_col=0) for file in pick_assignment_files]
+
+# Concatenate all DataFrames into a single DataFrame
+mycatalog_picks_merged = pd.concat(dfs)
+
+# Load networks
+networks = pd.read_csv('../data/networks.csv')
+
+# Define the channels to show in the plot
+channel = '?H?'
+
+fig_title = 'New Events (<5 km) from all regions'
+
+filepath = f'../data/datasets_{year}/plots_new_events_<5km.pdf'
+
+subplots_cluster_scale(new_events, mycatalog_picks_merged, networks, channel, fig_title,filepath)
